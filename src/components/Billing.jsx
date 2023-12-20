3ea9a47ba0bc81adfd47db3f1303a67a87621cb3
Savan <sawanpatel2508@gmail.com>
2023-12-20 16:04:07 -0500

    Create a business section

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,3 +1,9 @@
+import { layout } from "../style.js";
+
 export const Billing = () => {
-  return <></>;
+  return (
+    <section id="product" className={layout.sectionReverse}>
+      <div className={layout.sectionImgReverse}></div>
+    </section>
+  );
 };
