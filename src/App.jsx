6d654987034bf353da5b919c9a6eb18fb7240d13
Savan <sawanpatel2508@gmail.com>
2023-12-20 00:40:24 -0500

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,41 @@
-import "./App.css";
+import {
+  Business,
+  CardDeal,
+  CTA,
+  Footer,
+  Hero,
+  Navbar,
+  Stats,
+  Clients,
+  Testimonials,
+} from "./components/index.js";
+import styles from "./style.js";
 
-function App() {}
+function App() {
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats /> <Business /> <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default App;
---
+++ src/components/index.js
@@ -0,0 +1,25 @@
+import { Navbar } from "./Navbar.jsx";
+import { Footer } from "./Footer.jsx";
+import { Button } from "./Button.jsx";
+import { Billing } from "./Billing.jsx";
+import { Business } from "./Business.jsx";
+import { CardDeal } from "./CardDeal.jsx";
+import { Clients } from "./Clients.jsx";
+import { CTA } from "./CTA.jsx";
+import { Stats } from "./Stats.jsx";
+import { Testimonials } from "./Testimonials.jsx";
+import { Hero } from "./Hero.jsx";
+
+export {
+  Navbar,
+  Footer,
+  Button,
+  Hero,
+  Testimonials,
+  Stats,
+  CTA,
+  Clients,
+  CardDeal,
+  Business,
+  Billing,
+};
