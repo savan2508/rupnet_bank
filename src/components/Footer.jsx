f4a82fc538b15fd538c5018939c0f7ab03b37a5c
Savan <sawanpatel2508@gmail.com>
2023-12-20 22:49:47 -0500

    Include Footer.jsx component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,64 @@
+import styles from "../style.js";
+import { logo } from "../assets/index.js";
+import { footerLinks, socialMedia } from "../constants/index.js";
+
 export const Footer = () => {
-  return <></>;
+  let currentYear = new Date().getFullYear();
+  return (
+    <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
+      <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
+        <div className="flex-1 flex flex-col justify-start mr-10">
+          <img
+            src={logo}
+            alt="bank"
+            className="w-[266px] h-[72px] object-contain"
+          />
+          <p className={`${styles.paragraph} mt-4 max-x-[310px]`}>
+            A new way to make the payments easy, reliable and secure.
+          </p>
+        </div>
+        <div className="flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10">
+          {footerLinks.map((footerLink) => (
+            <div
+              key={footerLink.key}
+              className="flex flex-col ss:my-0 my-4 min-w-[150px]"
+            >
+              <h4 className="font-poppins font-medium text-[18px] leading-[27px] text-white">
+                {footerLink.title}
+              </h4>
+              <ul className="list-none mt-4">
+                {footerLink.links.map((link, index) => (
+                  <li
+                    key={link.name}
+                    className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer ${
+                      index !== footerLink.links.length - 1 ? "mb-4" : "mb-0"
+                    }`}
+                  >
+                    {link.name}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[#3f3e45]">
+        <p className="font-poppins font-normal text-[18px] leading-[27px] text-white">
+          {currentYear} © All Rights Reserved.{" "}
+        </p>
+        <div className="flex flex-row md:mt-4 mt-6">
+          {socialMedia.map((social) => (
+            <img
+              key={social.id}
+              src={social.icon}
+              alt={social.id}
+              className={`w-[21px] h-[21px] object-contain cursor-pointer ${
+                socialMedia.length !== social.id ? "mr-6" : "mr-0"
+              }`}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
